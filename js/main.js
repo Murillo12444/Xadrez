eee9d672661af50aa6612b99b2a0cd84fa95b6a1
Murillo12444 <murillo7865@gmail.com>
2025-05-21 15:17:42 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,286 @@
+// Referenciando o Canvas
+var canvas = document.getElementById('meu_canvas');
+
+// Obtendo o contexto gráfico
+var context = canvas.getContext('2d');
+
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(150, 50, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(50, 50, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(350, 50, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(250, 50, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(550, 50, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(450, 50, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(750, 50, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(650, 50, 100, 100);
+
+
+
+
+
+
+
+
+
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(150, 150, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(50, 150, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(350, 150, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(250, 150, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(550, 150, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(450, 150, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(750, 150, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(650, 150, 100, 100);
+
+
+
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(150, 250, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(50, 250, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(350, 250, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(250, 250, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(550, 250, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(450, 250, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(750, 250, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(650, 250, 100, 100);
+
+
+
+
+
+
+
+
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(150, 350, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(50, 350, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(350, 350, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(250, 350, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(550, 350, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(450, 350, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(750, 350, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(650, 350, 100, 100);
+
+
+
+
+
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(150, 450, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(50, 450, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(350, 450, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(250, 450, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(550, 450, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(450, 450, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(750, 450, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(650, 450, 100, 100);
+
+
+
+
+
+
+
+
+
+
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(150, 550, 100, 100);
+// Contorno azul, com espessura de 3px
+
+context.fillStyle = 'black';
+context.fillRect(50, 550, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(350, 550, 100, 100);
+
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(250, 550, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(550, 550, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(450, 550, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(750, 550, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(650, 550, 100, 100);
+
+
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(150, 650, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(50, 650, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(350, 650, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(250, 650, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(550, 650, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.strokeRect(450, 650, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.fillRect(750, 650, 100, 100);
+// Contorno azul, com espessura de 3px
+
+
+
+
+
+
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(150, 750, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(50, 750, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(350, 750, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(250, 750, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(550, 750, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(450, 750, 100, 100);
+
+// Preenchimento vermelho
+context.fillStyle = 'black';
+context.strokeRect(750, 750, 100, 100);
+// Contorno azul, com espessura de 3px
+context.fillStyle = 'black';
+context.fillRect(650, 750, 100, 100);
+
+
+context.fillStyle = 'black';
+context.strokeRect(50, 50, 800, 801);
+
+
+context.lineWidth = 15;
+context.fillStyle = 'black';
+context.strokeRect(25, 25, 850, 850);
+
+
+
+
